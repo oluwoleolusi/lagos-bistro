db605d78fdbde6242750e8fbda8415d696486eb3
Oluwole Olusi <oluwole.olusi@gmail.com>
2026-09-24 01:13:38 +0100

    Fixed Relative paths

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/demos/lagos-bistro">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
